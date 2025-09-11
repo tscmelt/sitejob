08175049cccb64de8546035cb93651602d4e18d7
Andriy Murhan <andrijmurgan@gmail.com>
2025-09-12 01:05:27 +0300

    burger-menu logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,10 @@
-const navbar = document.querySelector('.navbar');
 const menuIcon = document.querySelector('.menu-icon');
-const crossIcon = document.querySelector('.cross-menu-icon');
 const nav = document.querySelector('.navbar nav');
 
-menuIcon.addEventListener('click', () => {
-  navbar.classList.add('open');
-  nav.classList.add('show');
-});
+const body = document.body;
 
-crossIcon.addEventListener('click', () => {
-  navbar.classList.remove('open');
-  nav.classList.remove('show');
+menuIcon.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  menuIcon.classList.toggle('active');
+  body.classList.toggle('no-scroll');
 });
